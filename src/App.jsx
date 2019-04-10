f97efe6756815dc959002f69011a893bf3c4f952
Rogatnev Nikita <mail@rogatnev.ru>
2019-04-10 16:40:09 +0300

    base react app init

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import Routes from './Routes';
 
 const App = () => (
     <BrowserRouter>
-        <main className="container">
-            <div>
-                <h1>hello world!</h1>
-                <p>If you see this everything is working!</p>
-            </div>
-            <ul className="left">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
+        <main className="main">
+            <header className="header">
+                <h1 className="header__logo">Social Feed React App</h1>
+
+                <nav className="header__navigation">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
             <Routes/>
         </main>
     </BrowserRouter>
---
+++ src/components/widget.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Widget extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                12
+            </div>
+        );
+    }
+}
+
+export default Widget;
